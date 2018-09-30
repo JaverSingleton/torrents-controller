4d4b4a27d8155379fecd0b1c1aadad84fa113de7
poignatov <poignatov@avito.ru>
2018-09-30 14:34:45 +0300

    Initlal commit

---
+++ app/src/main/java/ru/worldtillfuture/torrents_controller/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.worldtillfuture.torrents_controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
